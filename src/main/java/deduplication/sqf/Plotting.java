26fb51a891d767d0cbcd974afe9a0638a9be081d
Caroline Gihlstorf <cgihlstorf@haverford.edu>
2021-04-29 18:57:44 -0400

    Finsihing fibonnaci method

--- src/main/java/deduplication/sqf/Plotting.java
+++ src/main/java/deduplication/sqf/Plotting.java
@@ -3,7 +3,7 @@ package deduplication.sqf;
 public class Plotting {
 	
 	public static void main(String args[]) {
-		for(int n = 0; n <= 100; n = n+40) {
+		for(int n = 0; n <= 100; n++) {
 			double start = startTimer();
 			int result = fib(n);
 			double end = endTimer();
@@ -20,10 +20,8 @@ public class Plotting {
 		if(n == 0 || n ==1)
 			return 1;
 		else {
-			System.out.println(n);
-			int num1 = fib(n-1);
-			int num2 = fib(n-2);
-			return num1 + num2;
+			//System.out.println(n);
+			return fib(n - 2) + fib(n - 1);
 		}
 	}
 	
